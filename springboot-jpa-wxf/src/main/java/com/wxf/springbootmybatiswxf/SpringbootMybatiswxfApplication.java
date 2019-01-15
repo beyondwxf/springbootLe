1f349e6cc7a634b48e6725074b5b2cfb7c7a6d6f
wangxuefei <wangxuefei@staff.hexun.com>
2019-01-15 17:18:45 +0800

    springbootMybites学习

---
+++ springboot-jpa-wxf/src/main/java/com/wxf/springbootmybatiswxf/SpringbootMybatiswxfApplication.java
@@ -0,0 +1,14 @@
+package com.wxf.springbootmybatiswxf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatiswxfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatiswxfApplication.class, args);
+    }
+
+}
+
---
+++ springbootmybeatswxf/src/main/java/com/wxf/springbootmybeatswxf/SpringbootmybeatswxfApplication.java
@@ -0,0 +1,14 @@
+package com.wxf.springbootmybeatswxf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmybeatswxfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootmybeatswxfApplication.class, args);
+    }
+
+}
+
