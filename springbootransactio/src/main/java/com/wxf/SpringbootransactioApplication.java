32a36a704489724696d71ceda1c3585cb7cccf78
wangxuefei <wangxuefei@staff.hexun.com>
2019-01-16 17:51:23 +0800

    springbootMybitestx学习

---
+++ springbootransactio/src/main/java/com/wxf/SpringbootransactioApplication.java
@@ -0,0 +1,14 @@
+package com.wxf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootransactioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootransactioApplication.class, args);
+    }
+
+}
+
